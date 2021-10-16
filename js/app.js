1c16a13340f3c0bf600fcc62044f1a864e56cbda
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-10-16 00:37:27 -0400

    refactor and add comments

--- js/app.js
+++ js/app.js
@@ -1,20 +1,5 @@
 /**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Define Global Variables
+ * Global Variables
  *
  */
 let timer = null; // used to clear out settimeout
@@ -24,14 +9,25 @@ let timer = null; // used to clear out settimeout
  * Start Helper Functions
  *
  */
+
+/**
+ * @description activates sections and nav items
+ * @param section - page's section
+ * @param navLink - a navmenu item
+ */
 const activateElements = function (section, navLink) {
-  section.classList.add("your-active-class");
-  navLink.classList.add("active");
+  section.classList.add('your-active-class');
+  navLink.classList.add('active');
 };
 
+/**
+ * @description Deactivates sections and nav items
+ * @param section - page's section
+ * @param navLink - a navmenu item
+ */
 const dactivateElements = function (section, navLink) {
-  section.classList.remove("your-active-class");
-  navLink.classList.remove("active");
+  section.classList.remove('your-active-class');
+  navLink.classList.remove('active');
 };
 
 /**
@@ -40,57 +36,66 @@ const dactivateElements = function (section, navLink) {
  *
  */
 
-// build the nav
+/**
+ * @description Builds the navbar
+ */
 const buildNav = function () {
-  const sections = document.querySelectorAll(".landing__container");
-  const list = document.querySelector("#navbar__list");
+  const sections = document.querySelectorAll('.landing__container');
+  const list = document.querySelector('#navbar__list');
 
   for (const section of sections) {
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
-    link.classList.add("menu__link");
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.classList.add('menu__link');
     link.innerHTML = section.firstElementChild.textContent;
     listItem.appendChild(link);
     list.appendChild(listItem);
   }
-  list.style.cssText = "display: flex; justify-content: flex-end;";
+  list.style.cssText = 'display: flex; justify-content: flex-end;';
 };
 
-// Add class 'active' to section when near top of viewport
+/**
+ * @description Add class 'active' to section when near top of viewport
+ */
 const activateSection = function () {
-  const sections = document.getElementsByTagName("section");
-  const navLinks = document.querySelectorAll(".menu__link");
+  const sections = document.getElementsByTagName('section');
+  const navLinks = document.querySelectorAll('.menu__link');
 
-  window.addEventListener("scroll", function () {
+  window.addEventListener('scroll', function () {
     for (let i = 0; i < sections.length; i++) {
       const distance = sections[i].getBoundingClientRect();
 
-      if (distance.top <= 400 && distance.top > -300)
+      if (distance.top <= 400 && distance.top > -350)
         activateElements(sections[i], navLinks[i]);
       else dactivateElements(sections[i], navLinks[i]);
     }
   });
 };
 
-// Scroll to anchor ID using scrollTO event
+/**
+ * @description Scroll to section when clicking on the navbar item
+ */
 const scrollToSection = function () {
-  const links = document.querySelectorAll(".menu__link");
+  const links = document.querySelectorAll('.menu__link');
   for (const [i, link] of links.entries()) {
-    link.addEventListener("click", function () {
+    link.addEventListener('click', function () {
       const section = document.getElementById(`section${i + 1}`);
       section.scrollIntoView();
     });
   }
 };
 
-// hide navbar when not scrolling
+/**
+ * @description Hides navbar when not scrolling
+ */
 const hideNavbar = function () {
-  window.addEventListener("scroll", function () {
-    document.querySelector("#navbar__list").style = "flex";
+  window.addEventListener('scroll', function () {
+    document.querySelector('#navbar__list').style = 'flex';
+    // if the timer equals a value, cancel the callback function of the settimeout()
     if (timer !== null) clearTimeout(timer);
     timer = setTimeout(function () {
-      document.querySelector("#navbar__list").style.display = "none";
-    }, 1000);
+      document.querySelector('#navbar__list').style.display = 'none';
+    }, 2000);
   });
 };
 
